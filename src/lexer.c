b9d15dcce249865547a090e0996d6b9d8e97eed6
Ian Li <ianli@ianli.gq>
2020-04-11 16:17:43 -0400

    comments

--- src/lexer.c
+++ src/lexer.c
@@ -5,13 +5,21 @@
 #define LEXER_TOKEN_UNIT_SIZE 7
 /* TODO optimize this number */
 
+/*
+ * lexes the input string and replaces the output variable
+ * with an array of strings of each individual token on its
+ * own. Then returns the number of tokens lexed. self reminder
+ * to free the memory after you're done lol
+ */
 int lexer (char* input, char*** output)
 {
-        char** res = malloc(sizeof(char *)); /* one token */
-        int token_id = 0;
-        int token_maxsize = 0;
-        int token_len = 0;
+        /* make temp. list to fill */
+        char** res = malloc(sizeof(char *)); /* space for one token */
+        int token_id = 0; /* which token code is currently on */
+        int token_maxsize = 0; /* how much memory the token has right now */
+        int token_len = 0; /* actual index of the string the token is at */
 
+        /* for every char in input */
         for (int i = 0; i<strlen(input); i++) {
                 if (input[i] == ' ' ||
                     input[i] == '\n') 
@@ -28,7 +36,8 @@ int lexer (char* input, char*** output)
                         if (token_len >= token_maxsize) {
                                 token_maxsize += LEXER_TOKEN_UNIT_SIZE;
                                 res[token_id] = realloc(res[token_id],
-                                                        token_maxsize * sizeof(char));
+                                                        token_maxsize
+                                                        * sizeof(char));
                         }
                         /* fill token with char from input */
                         res[token_id][token_len] = input[i];
@@ -36,6 +45,7 @@ int lexer (char* input, char*** output)
                 }
         }
 
+        /* replaces the variable provided with result */
         *output = res;
         /* returns length */
         return token_id + 1;
@@ -53,6 +63,8 @@ int main (int argc, char** argv)
         int len = lexer(argv[1], &res);
         for (int i = 0; i<len; i++)
                 printf("%s$\n", res[i]);
+        free(res);
+
         /* 
          * note to self: reminder to only lex upon these characters:
          * - literal space - CHECK
